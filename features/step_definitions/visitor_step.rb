c6f3e4769c582736d7c569115deb07e9b792776c
Przemek <przemek@krowinski.eu>
2015-05-09 16:00:28 +0200

    Add strps for registration and welcome page browsing

---
+++ features/step_definitions/visitor_step.rb
@@ -0,0 +1,11 @@
+Given(/^I'm not logged in$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I visit a registration page$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the registration form$/) do
+    pending # express the regexp above with the code you wish you had
+end

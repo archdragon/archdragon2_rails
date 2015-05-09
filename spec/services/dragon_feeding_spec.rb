27eaa31f9201a5b1c4c8a4a2b83d96dd30cbbda2
Przemek <przemek@krowinski.eu>
2015-05-09 17:34:45 +0200

    Expect success

--- spec/services/dragon_feeding_spec.rb
+++ spec/services/dragon_feeding_spec.rb
@@ -1,15 +1,18 @@
+require_relative '../../lib/arch_service/arch_service'
 require_relative '../../app/services/dragon_feeding_service'
 require_relative '../../app/models/dragon'
+
 describe DragonFeedingService do
   # How to describ services, when they have .call only?
 
   FoodItem = Class.new
 
   context "with proper dragon and food" do
-    let(:dragon) { Dragon.new }
-    let(:foodItem)   { FoodItem.new }
+    let(:dragon)   { Dragon.new }
+    let(:foodItem) { FoodItem.new }
     it "can feed the dragon only once per day" do
       feeding_service = DragonFeedingService.call!(dragon: dragon, foodItem: foodItem)
+      expect(feeding_service).to be_kind_of(ArchService::Response::Success)
     end
   end
 end

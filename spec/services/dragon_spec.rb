818e7c63b8bf18b1248f2d52d08cb354051a3d56
Przemek <przemek@krowinski.eu>
2015-05-09 16:30:01 +0200

    Merge

--- spec/services/dragon_spec.rb
+++ spec/services/dragon_spec.rb
@@ -1,13 +1,16 @@
 describe DragonFeedingService do
   # How to describ services, when they have .call only?
+
+  Dragon = Class.new
+  Food = Class.new
+
   context "with proper dragon and food" do
     before do
       @dragon = Dragon.new
       @food = Food.new
     end
     it "can feed the dragon only once per day" do
-      feeding_service = DragonFeedingService.new(dragon: dragon, food: food)
-      feeding_service.call!
+      feeding_service = DragonFeedingService.call!(dragon: dragon, food: food)
     end
   end
 end

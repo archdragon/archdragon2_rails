b7494ce6c0143c8d64e9fb4d93cd680a86207dea
Przemek <przemek@krowinski.eu>
2015-05-12 16:46:29 +0200

    Add basic dragon feeding

--- app/controllers/dragons_controller.rb
+++ app/controllers/dragons_controller.rb
@@ -1,8 +1,9 @@
 class DragonsController < ApplicationController
   def feed
-    DragonFeedingForm.call!(
+    feeding_form = DragonFeedingForm.new(
       dragon: Dragon.new(user: User.new),
       food_item: FoodItem.new
     )
+    feeding_form.submit
   end
 end

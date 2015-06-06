fec0de618290f07b33d5abc91e8e9083fc1070ab
Przemek <przemek@krowinski.eu>
2015-06-06 23:42:16 +0200

    Add users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :set_authentication_token
+
+  def set_authentication_token
+    return if authentication_token.present?
+    self.authentication_token = generated_authentication_token
+  end
+
+  def generated_authentication_token
+    SecureRandom.uuid.gsub(/\-/,'')
+  end
 end

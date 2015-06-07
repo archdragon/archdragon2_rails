546cc449d1eedccbb69f741576480f1441a19c12
Przemek <przemek@krowinski.eu>
2015-06-07 22:09:01 +0200

    Add session resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   get '/welcome/index2', controller: 'welcome', action: 'index2'
 
+  resource :session
+
   resources :dragons do
     member do
       post 'feed'
